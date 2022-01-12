0592f727e05af1a3dd1888b8a0409b990f17e35e
gabrielodom <gabriel.odom@fiu.edu>
2022-01-12 11:56:15 -0500

    Adding workflow to README

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ date: 2021-08-03
 ## Introduction
 We have wrangled, cleaned, and extracted information from the CDC's [open data repository](https://healthdata.gov/Health/COVID-19-Community-Profile-Report/gqxm-d9w9) related to the Delta variant of COVID-19. Our aim is to better display the state of the pandemic in the South Florida region of Miami-Dade, Broward, and Palm Beach counties.
 
+
+## Our Processs
+In order to replicate our work, here are the following steps:
+
+1. Go to <https://healthdata.gov/Health/COVID-19-Community-Profile-Report/gqxm-d9w9> and download the `.xlsx` files for the dates of interest. We check for new data files daily. 
+2. Save the selected data files to `data_CDC_raw/` (do not change the file names; they should all be in the form "Community_Profile_Report_[YYYYMMDD]_Public.xlsx").
+3. Wrangle all the data files in the `data_CDC_raw/` directory: the most recent script (as of 12 January 2022) is `scripts/wrangle_CDC_20210804.R`.
+
---
+++ scripts/wrangle_CDC_20220112.R
@@ -0,0 +1,199 @@
+# Inspect CDC Data
+# Gabriel Odom and Anny Rodriguez
+# 2021-08-03 - Updated 2022-01-12
+
+library(readxl)
+library(lubridate)
+library(tidyverse)
+
+# Data source:
+# https://healthdata.gov/Health/COVID-19-Community-Profile-Report/gqxm-d9w9
+
+
+
+######  Apply to All Data  ####################################################
+
+###  Vector of Filenames  ###
+allFilePaths_char <- paste0("data_CDC_raw/", list.files("data_CDC_raw/"))
+names(allFilePaths_char) <- paste0(
+	"date_",
+	str_extract(
+		allFilePaths_char, pattern = "[:digit:]{8}"
+	)
+)
+
+
+###  Columns of Interest  ###
+colsOfInterest <- c(
+	# Demographics
+	"County",
+	"State Abbreviation",
+	"Population",
+	# Cases, Testing, and Deaths
+	"Cases - last 7 days",
+	"Cumulative cases",
+	"Viral (RT-PCR) lab test positivity rate - last 7 days (may be an underestimate due to delayed reporting)",
+	"Total RT-PCR diagnostic tests - last 7 days (may be an underestimate due to delayed reporting)",
+	"NAAT positivity rate - last 7 days (may be an underestimate due to delayed reporting)",
+	"Total NAATs - last 7 days (may be an underestimate due to delayed reporting)",
+	# Hospitalizations
+	"Confirmed COVID-19 admissions - last 7 days",
+	"Suspected COVID-19 admissions - last 7 days",
+	"% inpatient beds occupied by COVID-19 patient",
+	"Total inpatient beds among hospitals reporting - last 7 days",
+	"% ventilators in use by COVID-19 patient",
+	"Total ventilators among hospitals reporting - last 7 days",
+	"Deaths - last 7 days",
+	"Cumulative deaths",
+	# Vaccinations
+	"People with at least 1 dose",
+	"People who are fully vaccinated",
+	"People with at least 1 dose - ages 65+",
+	"People who are fully vaccinated - ages 65+",
+	"People who are fully vaccinated as % of population - ages 65+",
+	"People with at least 1 dose - 12-17",
+	"People who are fully vaccinated - 12-17",
+	"People who are fully vaccinated as % of population - 12-17",
+	"People who have received a booster dose since August 13, 2021",
+	"People who have received a booster dose as % of fully vaccinated population"
+)
+
+# Misspellings
+otherNames_char <- c(
+	# Only for 17 December
+	"Viral (RT-PCR) lab test positivity rate - last 7 days (may exhibit anomalies due to delayed reporting)",
+	# effective June 6
+	"People with at least 1 dose - ages 12-17",
+	"People who are fully vaccinated - ages 12-17",
+	"People who are fully vaccinated as % of population - ages 12-17"
+)
+
+														
+														
+t0 <- Sys.time()
+allData_ls <- 
+	map(
+		.x = allFilePaths_char,
+		.f = ~{
+			read_excel(
+				path = .x, 
+				sheet = "Counties",
+				skip = 1
+			) %>% 
+				select(
+					any_of(c(colsOfInterest, otherNames_char))
+				) %>% 
+				filter(`State Abbreviation` == "FL")
+		}
+	)
+t1 <- Sys.time()
+t1 - t0
+# 1.285412 min
+
+allData_df <- bind_rows(allData_ls, .id = "DataSet")
+
+
+
+######  Clean Up Columns  #####################################################
+cleanData_df <- 
+	allData_df %>% 
+	###  Duplicated Viral PCR Testing  ###
+	rename(
+		`Viral (RT-PCR) lab test positivity rate - last 7 days` = 
+			`Viral (RT-PCR) lab test positivity rate - last 7 days (may be an underestimate due to delayed reporting)`
+	) %>% 
+	mutate(
+		`Viral (RT-PCR) lab test positivity rate - last 7 days` = case_when(
+			# If the old column name doesn't have missing data, move it to the new
+			#   column
+			!is.na(`Viral (RT-PCR) lab test positivity rate - last 7 days (may exhibit anomalies due to delayed reporting)`) ~
+				`Viral (RT-PCR) lab test positivity rate - last 7 days (may exhibit anomalies due to delayed reporting)`,
+			# If the new new column is not missing data, keep it
+			!is.na(`Viral (RT-PCR) lab test positivity rate - last 7 days`) ~
+				`Viral (RT-PCR) lab test positivity rate - last 7 days`
+		)
+	) %>% 
+  ### Using the NAAT positivity rate instead of the PCR ###
+  rename(
+    `NAAT positivity rate - last 7 days` = 
+      `NAAT positivity rate - last 7 days (may be an underestimate due to delayed reporting)`
+  ) %>% 
+  mutate(
+    `Positivity Rate` = case_when(
+      # If the old column (PCR) doesn't have missing data, move it #
+      is.na(`Viral (RT-PCR) lab test positivity rate - last 7 days`) ~ 
+        `NAAT positivity rate - last 7 days`,
+      # If the new colum is not missing data, keep it #
+      !is.na(`Viral (RT-PCR) lab test positivity rate - last 7 days`) ~
+        `Viral (RT-PCR) lab test positivity rate - last 7 days`
+    )
+  ) %>% 
+	# ###  Duplicated Vaccination 12-17: One Dose  ###
+	# # "People with at least 1 dose - ages 12-17"
+	# mutate(
+	# 	`People with at least 1 dose - 12-17` = case_when(
+	# 		# If the old column name doesn't have missing data, move it to the new
+	# 		#   column
+	# 		!is.na(`People with at least 1 dose - ages 12-17`) ~
+	# 			`People with at least 1 dose - ages 12-17`,
+	# 		# If the new new column is not missing data, keep it
+	# 		!is.na(`People with at least 1 dose - 12-17`) ~
+	# 			`People with at least 1 dose - 12-17`
+	# 	)
+	# ) %>% 
+	# select(
+	# 	-`People with at least 1 dose - ages 12-17`
+	# ) %>%
+	
+	###  Duplicated Vaccination 12-17: Fully Vaccinated  ###
+	# "People who are fully vaccinated - ages 12-17"
+	mutate(
+		`People who are fully vaccinated - 12-17` = case_when(
+			# If the old column name doesn't have missing data, move it to the new
+			#   column
+			!is.na(`People who are fully vaccinated - ages 12-17`) ~
+				`People who are fully vaccinated - ages 12-17`,
+			# If the new new column is not missing data, keep it
+			!is.na(`People who are fully vaccinated - 12-17`) ~
+				`People who are fully vaccinated - 12-17`
+		)
+	) %>% 
+	select(
+		-`People who are fully vaccinated - ages 12-17`
+	) %>%
+	
+	###  Duplicated Vaccination 12-17: Fully Vaccinated %  ###
+	# "People who are fully vaccinated as % of population - ages 12-17"
+	mutate(
+		`People who are fully vaccinated as % of population - 12-17` = case_when(
+			# If the old column name doesn't have missing data, move it to the new
+			#   column
+			!is.na(`People who are fully vaccinated as % of population - ages 12-17`) ~
+				`People who are fully vaccinated as % of population - ages 12-17`,
+			# If the new new column is not missing data, keep it
+			!is.na(`People who are fully vaccinated as % of population - 12-17`) ~
+				`People who are fully vaccinated as % of population - 12-17`
+		)
+	) %>% 
+	select(
+		-`People who are fully vaccinated as % of population - ages 12-17`
+	) %>%
+	
+	###  Fix Dates  ###
+	mutate(
+		Date_char = str_remove(DataSet, "date_")
+	) %>% 
+	mutate(
+		Date = ymd(Date_char)
+	) %>% 
+	select(-DataSet, -Date_char) %>% 
+	select(Date, everything())
+
+
+write_csv(
+	cleanData_df, "data_clean/cleaned_CDC_COVID_data_20220110.csv"
+)
+
+# Restart here
+
+
